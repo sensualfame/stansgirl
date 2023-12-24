32319e24a4a572861ef1af186d52f2e7b53e6ea3
sensualfame <149739801+sensualfame@users.noreply.github.com>
2023-12-25 08:10:30 +1000

    Add files via upload

--- js/api.js
+++ js/api.js
@@ -1,17 +1,52 @@
-const main = async () => {
-  const results = await fetch('https://api.ipdata.co?api-key=a84a536211b7f8d838608979cbfa046317ca14c1d2f3425dd1db396a');
-  
-  // Variable that stores the data as a Json file.
-  const data = await results.json();
-  // City extracted from Json file.
-  const city = data.city; 
-  const country = data.country;
+import { API_KEYS } from './keys.js';
+// Fetch client's IP using httpbin
+$.get("https://httpbin.org/ip", function(data) {
+    (async function() {
+        for (let key of API_KEYS) {
+            if (await updateLocation(data.origin, key)) {
+                break; // Exit loop if location update is successful
+            }
+        }
+    })();
+}, "json");
 
-  document.getElementById("city").innerHTML = city;
-  document.getElementById("cityFooter").innerHTML = city;
+function updateLocationDetails(response) {
+    // Update country and city
+    $("#country_name").html(response.country_name);
+    $("#city").html(response.city);
 
+    // Determine the location display string
+    const locationDisplay = response.city ? `${response.city}, ${response.country_name}` : response.country_name;
+
+    // Update location information in HTML
+    $(".location").html(locationDisplay);
+    $("#location").html(locationDisplay);
+
+    // Create coordinates string
+    const coords = `${response.latitude},${response.longitude}`;
+
+    // Generate the image URL for the map
+    const imgUrl = `<img src="https://maps.googleapis.com/maps/api/staticmap?center=${coords}&zoom=14&size=400x300&sensor=false&key=YOUR_GOOGLE_MAPS_API_KEY&markers=color:red%7C${coords}">`;
+
+    // Update the map holder with the new map image
+    $("#mapholder").html(imgUrl);
 }
 
-main();
-  
+function generateGeoApiProxyUrl(ip, token) {
+    // Construct the CORS Proxy URL for the geolocation API
+    const geoApiUrl = `https://api.ipgeolocation.io/ipgeo?apiKey=${token}&ip=${ip}`;
+    return `https://corsproxy.io/?${encodeURIComponent(geoApiUrl)}`;
+}
 
+function updateLocation(ip, token) {
+    return new Promise((resolve, reject) => {
+        let proxyUrl = generateGeoApiProxyUrl(ip, token);
+
+        $.get(proxyUrl, function(response) {
+            updateLocationDetails(response);
+            resolve(true);
+        }, "json").fail(function(jqXHR) {
+            resolve(false);
+        });
+    });
+}
